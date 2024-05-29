5bc2ee7b107ca48749ec41042c4c86479505ae71
Phibo <phibo96@gmail.com>
2024-05-29 20:15:34 +0200

    function call should not be in try

--- src/game/connection/incoming/incomingMesseges.ts
+++ src/game/connection/incoming/incomingMesseges.ts
@@ -7,16 +7,19 @@ import { IMessage } from "../messageTypes.js";
 
 
 export function incomingMessages(characterName: string, msg: string) {
+  let validMsg: IMessage | undefined = undefined
   try {
     const message = JSON.parse(msg)
     console.log('received from %s: %s', characterName, JSON.stringify(message));
     if (!message || !message.type) {
       throw new Error('Invalid message, has no "type" property!');
     }
-
-    routeMessage(characterName, message)
+    validMsg = message
   } catch (error) {
     console.log('Incoming message, not a JSON! Should have a "type" property.');
+  }
+  if(validMsg){
+    routeMessage(characterName, validMsg)
   }
 }
 
